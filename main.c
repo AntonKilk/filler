8857c1d758b2b0b5f8510715c6dd13a037c4ea60
AntonKilk <ant.kilk@gmail.com>
2022-07-14 16:11:14 +0300

    leaks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akilk <akilk@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 14:43:53 by akilk             #+#    #+#             */
-/*   Updated: 2022/07/14 13:28:28 by akilk            ###   ########.fr       */
+/*   Updated: 2022/07/14 16:08:30 by akilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,23 @@ void	freeall(t_game *game, t_token *token)
 	i = 0;
 	if (game->board)
 	{
-		while (i < game->height)
+		while (game->board[i])
+		{
+			printf("in board\n");
 			free(game->board[i++]);
+		}
+		//ft_strdel(game->board);
 	}
-	ft_strdel(game->board);
 	i = 0;
 	if (token->map)
 	{
-		while (i < game->height)
+		while (token->map[i])
+		{
+			printf("in token\n");
 			free(token->map[i++]);
+		}
+		//ft_strdel(token->map);
 	}
-	ft_strdel(token->map);
 }
 
 int	error(char **str, char *msg)
@@ -67,26 +73,21 @@ int	main(void)
 		fprintf(stderr, "error initializing data\n");
 		return (0);
 	}
-	fprintf(stderr, "me: %c\n", game.me);
-	while (1)
+	while (!game.ended)
 	{
-		fprintf(stderr, "main starts: line %d\n", __LINE__);
 		if (!read_data(&game, &token))
 		{
-			fprintf(stderr, "return from main: line %d\n", __LINE__);
+			freeall(&game, &token);
 			return (0);
 		}
-		if (game.ended)
-			break ;
+		// if (game.ended)
+		// 	break ;
 		if (try_solve(&game, &token))
-		{
 			print_coor(game.result.y, game.result.x);
-			fprintf(stderr, "in main: %d %d\n", game.result.y, game.result.x);
-		}
 		else
 			break ;
 	}
-	//freeall(&game, &token);
+	freeall(&game, &token);
 	print_coor(0, 0);
 	return (0);
 }
